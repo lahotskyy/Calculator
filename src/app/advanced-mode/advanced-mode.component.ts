d3cd0d42c3a82fcc8891cc09173b9c9e46d39f03
lahotskyy <taras.lahotskyy@gmail.com>
2017-05-07 19:34:25 +0300

    add routing between components

---
+++ src/app/advanced-mode/advanced-mode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advanced-mode',
+  templateUrl: './advanced-mode.component.html',
+  styleUrls: ['./advanced-mode.component.css']
+})
+export class AdvancedModeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/simple-mode/simple-mode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-mode',
+  templateUrl: './simple-mode.component.html',
+  styleUrls: ['./simple-mode.component.css']
+})
+export class SimpleModeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
